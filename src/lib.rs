166329838745cab28c9124a1c3ab33b840279bdd
Ville-Matti Hakanpää <villematti@villematti.info>
2022-07-13 00:02:21 +0300

    Add case insensitive function

--- src/lib.rs
+++ src/lib.rs
@@ -40,18 +40,50 @@ impl Config {
     }
 }
 
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line.trim());
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let content = "\
         Rust:
         safe, fast, productive
-        Pick three.";
+        Pick three.
+        Duck tape.";
 
         assert_eq!(vec!["safe, fast, productive"], search(query, content));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+        Rust:
+        safe, fast, productive
+        Pick three.
+        Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
